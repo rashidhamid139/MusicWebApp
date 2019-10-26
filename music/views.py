7fead8188f93374a10a113e7b35759a652bcccd3
Rashid <rashidhamid139@gmail.com>
2019-10-26 15:45:24 +0530

    This is my 3rd commit

--- music/views.py
+++ music/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from  .models import Album
 # Create your views here.
 
-def index(requests):
+def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' +str(album.id)+'/'
-        html += '<a href="' + url + '">' + album.album_title +'</a><br>'
-    return  HttpResponse(html)
+    return  render(request, 'music/index.html', {'all_albums': all_albums,})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: "+str(album_id)+"</h2>")
+
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album':album})
